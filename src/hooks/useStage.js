b38e6ee2494b869a4e8a8d692891a133f06135d7
Lesley Boyd <lesley.a.boyd1@gmail.com>
2019-08-20 20:24:41 -0500

    added drop interval

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -9,16 +9,15 @@ export const useStage = (player, resetPlayer) => {
     setRowsCleared(0);
 
     const sweepRows = newStage =>
-        newStage.reduce((ack, row) => {
-            if(row.findIndex(cell => cell[0] === 0) === -1) {
-                setRowsCleared(prev => prev + 1);
-                ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
-                return ack;
-            }
-            ack.push(row);
-            return ack;
-        }, [])
-
+      newStage.reduce((ack, row) => {
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+          setRowsCleared(prev => prev + 1);
+          ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+          return ack;
+        }
+        ack.push(row);
+        return ack;
+      }, [])
 
     const updateStage = prevStage => {
       // First flush the stage
@@ -37,17 +36,16 @@ export const useStage = (player, resetPlayer) => {
           }
         });
       });
-    //   then check if we collided
-    if(player.collided){
+      // Then check if we collided
+      if (player.collided) {
         resetPlayer();
         return sweepRows(newStage);
-    }
-
+      }
       return newStage;
     };
 
     setStage(prev => updateStage(prev));
-  }, [player]);
+  }, [player, resetPlayer]);
 
   return [stage, setStage, rowsCleared];
 };
